40ee32415d0adf20038e8f29d27f46e73b509414
byambadorj2 <byambadorj993@gmail.com>
2026-01-12 19:20:55 +0800

    [chore] init, setup, product

---
+++ app/ProductsList.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Product } from "./product-data";
+
+export default function ProductsList({ products }: { products: Product[] }) {
+  return (
+    <div>
+      {products.map((product) => (
+        <Link key={product.id} href={"/product-detail"}>
+          <Image
+            src={"/" + product.imageUrl}
+            alt={product.name}
+            width={140}
+            height={150}
+          />
+          <h2>{product.name}</h2>
+          <p>Price: ${product.price.toFixed(2)}</p>
+          <p>{product.description}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
