e80d32450e9077d7f262c9a722e1b2534fb8aac3
byambadorj2 <byambadorj993@gmail.com>
2026-01-27 20:08:19 +0800

    Rewriting the add-to-cart endpoint

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from "next/server";
 import { products } from "@/app/product-data";
+import { connectToDb } from "@/app/api/db";
 
 type ShoppingCart = Record<string, string[]>;
 
@@ -16,10 +17,11 @@ export async function GET(
   request: NextRequest,
   { params }: { params: Promise<Params> },
 ) {
-  const { id: userId } = await params;
-  const productIds = carts[userId];
+  const { db } = await connectToDb();
+  const userId = (await params).id;
+  const userCart = await db.collection("carts").findOne({ userId });
 
-  if (productIds === undefined) {
+  if (!userCart) {
     return new Response(JSON.stringify([]), {
       status: 200,
       headers: {
@@ -28,9 +30,11 @@ export async function GET(
     });
   }
 
-  const cartProducts = productIds.map((id) =>
-    products.find((p) => p.id === id),
-  );
+  const cartIds = userCart.cartIds;
+  const cartProducts = await db
+    .collection("products")
+    .find({ id: { $in: cartIds } })
+    .toArray();
 
   return new Response(JSON.stringify(cartProducts), {
     status: 200,
@@ -48,14 +52,24 @@ export async function POST(
   request: NextRequest,
   { params }: { params: Promise<Params> },
 ) {
-  const { id: userId } = await params;
+  const { db } = await connectToDb();
+
+  const userId = (await params).id;
   const body: CartBody = await request.json();
   const productId = body.productId;
 
-  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
-  const cartProducts = carts[userId].map((id) =>
-    products.find((p) => p.id === id),
-  );
+  const updatedCart = await db
+    .collection("carts")
+    .findOneAndUpdate(
+      { userId },
+      { $push: { cartIds: productId } },
+      { upsert: true, returnDocument: "after" },
+    );
+
+  const cartProducts = await db
+    .collection("products")
+    .find({ id: { $in: updatedCart.cartIds } })
+    .toArray();
 
   return new Response(JSON.stringify(cartProducts), {
     status: 201,
