e0f6f160b89e1113074539eb66db12c2bf60c10c
byambadorj2 <byambadorj993@gmail.com>
2026-02-08 15:35:36 +0800

    Loading all products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,13 @@
 import ProductsList from "../ProductsList";
-import { products } from "../product-data";
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
+    {
+      cache: "no-store",
+    },
+  );
+  const products = await response.json();
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-4xl font-bold mb-8">Products</h1>
