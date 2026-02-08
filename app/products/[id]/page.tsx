c3b6000972efd73648752d6d5b9f03caf7132590
byambadorj2 <byambadorj993@gmail.com>
2026-02-08 15:35:19 +0800

    Loading individual products

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,4 @@
 import NotFoundPage from "@/app/not-found";
-import { products } from "@/app/product-data";
 import Image from "next/image";
 
 export default async function ProductDetailPage({
@@ -7,10 +6,15 @@ export default async function ProductDetailPage({
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const { id } = await params;
-  const product = products.find((p) => p.id === id);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products/${(await params).id}`,
+    {
+      cache: "no-store",
+    },
+  );
+  const productData = await response.json();
 
-  if (!product) {
+  if (!productData || !productData.id) {
     return <NotFoundPage />;
   }
 
@@ -19,17 +23,17 @@ export default async function ProductDetailPage({
       <div className="md:w-1/2 md:pr-8 mb-6 md:mb-0">
         <Image
           className="w-full h-auto rounded-lg shadow-md"
-          src={"/" + product.imageUrl}
-          alt={product.name}
+          src={"/" + productData.imageUrl}
+          alt={productData.name}
           width={100}
           height={100}
         />
       </div>
       <div className=" md:w-1/2">
-        <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
-        <p className="text-2xl text-gray-600 mb-6">${product.price}</p>
+        <h1 className="text-4xl font-bold mb-4">{productData.name}</h1>
+        <p className="text-2xl text-gray-600 mb-6">${productData.price}</p>
         <h1 className="text-2xl font-semibold mb-2">Description</h1>
-        <p className="text-gray-700">{product.description}</p>
+        <p className="text-gray-700">{productData.description}</p>
       </div>
     </div>
   );
